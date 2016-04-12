a04a83e95cb3bb70fabe128761ca301cbbef7891
TheNathanBlake <nathan.bs42@gmail.com>
2016-04-12 00:36:29 -0500

    let's fix this
    Thanks obama

--- beep.py
+++ beep.py
@@ -1,8 +1,16 @@
 import winsound
 
+# You used to debug on Ubuntu
 def drake():
 	freq = [659, 659, 659, 698, 659, 587, 523, 659, 523]
 	dur  = [240, 240, 240, 240, 240, 240, 240, 500, 960]
 
 	for i in range(0,len(freq)):
 		winsound.Beep(freq[i], dur[i])
+
+def music(sound):
+	winsound.PlaySound("%s.wav" %sound, winsound.SND_FILENAME)
+	
+# just in case you wanna run straight from command line
+#if __name__ == "__main__":
+	#music(sys.argv[1])
