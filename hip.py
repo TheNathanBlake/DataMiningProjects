f11fd0b4fda64cfb8fa885bfdfd2f8cee720f0d1
TheNathanBlake <nathan.bs42@gmail.com>
2016-04-12 01:43:47 -0500

    Edits, edits, more edits
    Removed the set_weight function, wrote functionality to the choose_att
    function
    
    modified the class again
    
    modified dtl to more closely resemble its final state

--- hip.py
+++ hip.py
@@ -4,17 +4,16 @@ from math import log2
 from math import log10
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
 from nltk.stem.snowball import SnowballStemmer
 
 t = time.time()
 
 stemmer = SnowballStemmer('english')
 
-df_train = pd.read_csv('data/train.csv', encoding="ISO-8859-1")
-df_test = pd.read_csv('data/test.csv', encoding="ISO-8859-1")
+df_train = pd.read_csv('../data/train.csv', encoding="ISO-8859-1")
+df_test = pd.read_csv('../data/test.csv', encoding="ISO-8859-1")
 # df_attr = pd.read_csv('../data/attributes.csv')
-df_pro_desc = pd.read_csv('data/product_descriptions.csv')
+df_pro_desc = pd.read_csv('../data/product_descriptions.csv')
 
 print("%f seconds reading" %(time.time() - t))
 
@@ -58,63 +57,64 @@ classifications = df_train['relevance'].values
 train_atts = df_train.drop(['id','relevance'],axis=1).values
 test_atts = df_test.drop(['id','relevance'],axis=1).values
 
+# to convert a number to a name: train_atts.columns.values.tolist()[x] -> name of column x
+
 def dtl(train_atts, depth, default):
 	if not train_atts:
 		return default
 	elif len(train_atts[0]) == 3: # id, product uid, relevance (with relevance as the class.  We also don't use uid as an att because it can cause overfitting)
-		return mode(train_atts)
-	else:
+		return (train_atts.values)
+	elif depth == 0:
+		return train_atts
+	else: # the condition that does not make a leaf
 		best_att = choose(train_atts) # the name of the best attribute, in string form
 		tree = node(best_att) # make a node with the best attribute
 		best_weight = tree.set_weight(train_atts) # get the best weight for the branch
-		for val in train_atts[best_att]: # probably not my best option
-			best_atts = # probably a threshold, determined by another function for thresholds for best separation (lowest avg. standard deviation)
+		train_left =  #fix soon
+		train_right = 0 #this too
+		tree.connect(\
+			dtl(train_left, depth-1, sum(train_left.values[2])/len(train_left.values)), \
+			dtl(train_right, depth-1, sum(train_right.values[2]/len(train_right.values))))
+		#for val in train_atts[best_att]: # probably not my best option
+			#best_atts = # probably a threshold, determined by another function for thresholds for best separation (lowest avg. standard deviation)
 		return tree
-		
+ 
 # changing a basic thing
 # node object (used with the decision tree learner)
 class node:
-	#define the threshold percentile step.
 	step = 10
-	def __init__(self, attribute):
+	#define the threshold percentile step.
+	def __init__(self, attribute, thresh):
 		self.attribute = attribute
+		self.thresh = thresh
 		self.branches = []
-	def connect(self, leaf):
-		self.branches.append(leaf)
+	def connect(self, leaf1, leaf2 = null):
+		self.branches.append(leaf1)
+		if leaf2:
+			self.branches.append(leaf2)
 
-def set_weight(values):
-	best = [False, False] # [threshold, variance] (looking for lowest variance sum, since high variance in a branch means rankings were less separated)
-	low=[]
-	high=[]
-	for weight in range(1,step+1):
-		L = min(values)
-		H = max(malues)
-		thresh = L + int(weight * (M-L)/(step+1)) # courtesy of the teachings from a particular AI book in the works cited folder
-		low = [i in values if i < thresh]
-		high = [i in values if i >= thresh]
-		low_mean = sum([i in low])/len(low)
-		high_mean = sum([i in high])/len(low)
-		low_var = sum([(i-low_mean)**2 for i in low]) / (len(low)-1)
-		high_var = sum([(i-high_mean)**2 for i in high] ) / (len(high)-1)
-
-		var = low_var + high_var
-		if not best[1] or var < best[1]:
-			best[0] = weight
-			best[1] = var
-	self.weight = best[0] #assign the best weight to the node
-
-def choose(train_atts):
+# chooses the best attribute for a particular branch
+def choose_att(train_atts, step):
 	max_gain = best_att = best_thresh = -1
-	for i in range(1,len(train_atts)):
-	
-#rf = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)
-#clf = BaggingRegressor(rf, n_estimators=45, max_samples=0.1, random_state=25)
-#clf.fit(X_train, y_train)
-#y_pred = clf.predict(X_test)
+	for A in range(2,len(train_atts)):
+		att_values = train_atts.values[A]
+		low = min(att_values)
+		high = max(att_values)
+		for weight in range(1,step+1):
+			thresh = low + int(weight * (high-low)/(step+1))
+			gain = infogain(train_atts, A, thresh) # i = attribute number
+			if gain > max_gain:
+				max_gain = gain
+				best_att = A
+				best_thresh = thresh
+	return [best_att, best_thresh]
+
+def variance(train_atts):
+	total = 0
 
 #pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv('submission.csv',index=False)\
 
 # returns the information gain of an attribute split based on provided threshold
-def infogain(entities):
+def infogain(train_atts, A, thresh):
 	entropy = 0
 	
