67fb932fe6786857690566d37034d728d7a948b3
TheNathanBlake <nathan.bs42@gmail.com>
2016-04-13 18:32:15 -0500

    Added sail()
    Some people don't like Drake :smirk:

--- beep.py
+++ beep.py
@@ -5,12 +5,18 @@ def drake():
 	freq = [659, 659, 659, 698, 659, 587, 523, 659, 523]
 	dur  = [240, 240, 240, 240, 240, 240, 240, 500, 960]
 
-	for i in range(0,len(freq)):
-		winsound.Beep(freq[i], dur[i])
+def sail():
+	freq = [659, 659, 659, 784, 784, 784, 880, 987, 880, 784, 659, 165]
+	dur  = [130, 130, 130, 130, 130, 130, 400, 400, 300, 130, 400, 700]
+
+	play(freq, dur)
 
 def music(sound):
 	winsound.PlaySound("%s.wav" %sound, winsound.SND_FILENAME)
-	
+
+def play(freq, dur):
+	for i in range(0,len(freq)):
+		winsound.Beep(freq[i], dur[i])
 # just in case you wanna run straight from command line
 #if __name__ == "__main__":
 	#music(sys.argv[1])
