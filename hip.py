9cc0627401f2099387d2afe28d3ad121412eda3b
TheNathanBlake <nathan.bs42@gmail.com>
2016-04-12 23:29:42 -0500

    Debugging functions
    Testing on a small subset of the data allowed me to further tweak the
    methods in the node object and the outside helper functions.

--- hip.py
+++ hip.py
@@ -1,13 +1,14 @@
 import beep #I want to know when it's done!
 import time
 from datetime import datetime
+
+print(datetime.now())
+
 from math import log2
 from math import log10
 import numpy as np
 import pandas as pd
 from nltk.stem.snowball import SnowballStemmer
-
-print(datetime.now())
 
 t = time.time()
 
@@ -15,21 +16,25 @@ node_count = 0
 
 #using 0.2 as a stopping variance
 def dtl(train_atts, depth, default):
+	global node_count
+	global att_names
+
 	node_count += 1
 	print("node %d, %d deep" %(node_count, depth))
 	if train_atts.values == []:
 		return node(None, default)
 	elif depth == 0:
 		return node(None, default)
-	elif variance(train_atts) > 0.002: # If the variance gets really small, we do this to keep us sane.
+	elif variance(train_atts) <= 0.002: # If the variance gets really small, we do this to keep us sane.
 		return node(None, default)
 	else: # the condition that does not make a leaf
 		tree = node(train_atts) # make a node with the best attribute
-		train_left  = train_atts[att_names[node.attribute].values]
-		train_right = train_atts[train_atts.columns.values[node.attribute]]
+		print(tree.attribute)
+		train_left  = train_atts[train_atts[tree.attribute] < tree.thresh]
+		train_right = train_atts[train_atts[tree.attribute] >= tree.thresh]#train_atts.columns.values[node.attribute]]
 		tree.connect(\
-			dtl(train_left, depth-1, sum(train_left.values[2])/len(train_left.values)), \
-			dtl(train_right, depth-1, sum(train_right.values[2]/len(train_right.values))))
+			dtl(train_left, depth-1, sum(train_left['relevance'])/len(train_left.values)), \
+			dtl(train_right, depth-1, sum(train_right['relevance']/len(train_right.values))))
 		return tree
  
 # changing a basic thing
@@ -39,6 +44,7 @@ class node:
 	branches = []
 	value = None
 	thresh = None
+	attribute = None
 	def connect(self, leaf1, leaf2 = None):
 		self.branches.append(leaf1)
 		if leaf2 is not None:
@@ -59,43 +65,49 @@ class node:
 					return traverse(branches[0])
 				else:
 					return traverse(branches[1])
-
-
 	def __init__(self, train_atts = None, val = None):
-		if train_atts is None: #this indicates there's a value
+		# print(self.step)
+		if train_atts is None or len(train_atts.values) == 0: #this indicates there's a value
 			if val is None:
 				print("Warning: Node object created without parameters.")
 			else:
 				self.value = val
 		else:
-			max_gain = self.attribute = self.thresh = -1
+			max_gain = self.thresh = -1
+			global att_names
 			for A in att_names:
 				att_values = train_atts[A].values
 				low = min(att_values)
 				high = max(att_values)
 				for weight in range(1,self.step):
 					curr_thresh = low + int(weight * (high-low)/(self.step))
-					gain = infogain(train_atts, A, thresh) # i = attribute number
+					gain = infogain(train_atts, A, curr_thresh) # i = attribute number
+					print(max_gain)
+					print(gain)
 					if gain > max_gain:
 						max_gain = gain
 						self.attribute = A
 						self.thresh = curr_thresh
+			if self.attribute is None:
+				print("Attribute not being selected correctly.")
 
 # returns the variance of rankings provided by the system
 def variance(train_atts):
+	#print(len(train_atts.values))
+	if len(train_atts.values) == 0:
+		return 0
 	total = 0
-	mean = sum(train_atts.values[2])/len(train_atts.values[2])
-	var = [(val+mean)**2 for val in train_atts.values[2]]
-	return (1/(len(train_atts.values[2])-1)) * var
+	relevance = train_atts.relevance.values
+	mean = sum(relevance)/len(relevance)
+	var = sum([(val-mean)**2 for val in relevance]) # sample, not population
+	return (1/(len(relevance)-1)) * var
 
 # returns the 'information gain' of an attribute split based on provided threshold
-# (basically just the added variance, not an actual info gain formula)
+# (basically just the averages, not an actual info gain formula)
 def infogain(train_atts, A, thresh):
-	low = train_atts[train_atts[A] < thresh]
-	high = train_atts[train_atts[A] >= thresh]
-	return 1 - (variance(low) + variance(high))
-
-
+	low = train_atts[train_atts[A] < thresh][A]
+	high = train_atts[train_atts[A] >= thresh][A]
+	return sum(low)/(len(low)+1) + sum(high)/(len(high)+1)
 
 # ====================BEGIN=================== #
 stemmer = SnowballStemmer('english')
@@ -141,14 +153,18 @@ df_train = df_all.iloc[:num_train]
 df_test = df_all.iloc[num_train:]
 id_test = df_test['id']
 
+# separate out the attribute names into their own array to make parsing easier
 att_names = (df_train.drop(['id', 'product_uid', 'relevance'], axis=1)).columns.values
 print("%f seconds messing with the arrays" %(time.time() - t))
 
-classifications = df_train['relevance'].values
-train_atts = df_train.drop(['id','relevance'],axis=1).values
 test_atts = df_test.drop(['id','relevance'],axis=1).values
 
-beep.drake() #lets me know when the provided stuff is done
+tree = dtl(df_train, 6, 1)
+
+# classifications = df_train['relevance'].values
+# train_atts = df_train.drop(['id','relevance'],axis=1).values
+
+# beep.drake() #lets me know when the provided stuff is done
 
 
 
